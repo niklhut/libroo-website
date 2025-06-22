95b97443177cf31ce5d49929eb0df665db260487
niklhut <49069026+niklhut@users.noreply.github.com>
2025-06-22 21:13:54 +0200

    feat: add legal pages links

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -1,52 +1,14 @@
 <script setup lang="ts">
-const columns = [{
-  label: 'Resources',
-  children: [{
-    label: 'Help center'
-  }, {
-    label: 'Docs'
-  }, {
-    label: 'Roadmap'
-  }, {
-    label: 'Changelog'
-  }]
-}, {
-  label: 'Features',
-  children: [{
-    label: 'Affiliates'
-  }, {
-    label: 'Portal'
-  }, {
-    label: 'Jobs'
-  }, {
-    label: 'Sponsors'
-  }]
-}, {
-  label: 'Company',
-  children: [{
-    label: 'About'
-  }, {
-    label: 'Pricing'
-  }, {
-    label: 'Careers'
-  }, {
-    label: 'Blog'
-  }]
-}]
-
-const toast = useToast()
-
-const email = ref('')
-const loading = ref(false)
-
-function onSubmit() {
-  loading.value = true
-
-  toast.add({
-    title: 'Subscribed!',
-    description: 'You\'ve been subscribed to our newsletter.'
-  })
-}
+const items = [
+  {
+    label: 'Imprint',
+    to: 'https://niklhut.com/imprint'
+  },
+  {
+    label: 'Privacy Policy',
+    to: 'https://niklhut.com/privacy'
+  }
+]
 </script>
 
 <template>
@@ -59,67 +21,24 @@ function onSubmit() {
   />
 
   <UFooter :ui="{ top: 'border-b border-default' }">
-    <template #top>
-      <UContainer>
-        <UFooterColumns :columns="columns">
-          <template #right>
-            <form @submit.prevent="onSubmit">
-              <UFormField
-                name="email"
-                label="Subscribe to our newsletter"
-                size="lg"
-              >
-                <UInput
-                  v-model="email"
-                  type="email"
-                  class="w-full"
-                  placeholder="Enter your email"
-                >
-                  <template #trailing>
-                    <UButton
-                      type="submit"
-                      size="xs"
-                      label="Subscribe"
-                    />
-                  </template>
-                </UInput>
-              </UFormField>
-            </form>
-          </template>
-        </UFooterColumns>
-      </UContainer>
-    </template>
-
     <template #left>
       <p class="text-sm text-muted">
         Copyright © {{ new Date().getFullYear() }}. All rights reserved.
       </p>
     </template>
 
+    <UNavigationMenu
+      :items="items"
+      variant="link"
+    />
+
     <template #right>
       <UButton
-        to="#"
-        target="_blank"
-        icon="i-simple-icons-discord"
-        aria-label="Discord"
+        icon="simple-icons:github"
+        variant="link"
         color="neutral"
-        variant="ghost"
-      />
-      <UButton
-        to="#"
+        to="https://github.com/niklhut"
         target="_blank"
-        icon="i-simple-icons-x"
-        aria-label="X"
-        color="neutral"
-        variant="ghost"
-      />
-      <UButton
-        to="https://github.com/nuxt-ui-pro/landing"
-        target="_blank"
-        icon="i-simple-icons-github"
-        aria-label="GitHub"
-        color="neutral"
-        variant="ghost"
       />
     </template>
   </UFooter>
