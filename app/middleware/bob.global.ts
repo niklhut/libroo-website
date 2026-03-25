2eb12d5ca6ff2753d05c4d6d525990108fa52c14
niklhut <49069026+niklhut@users.noreply.github.com>
2026-03-24 21:54:12 -0500

    refactor: improve 'itsmebob' query parameter handling

---
+++ app/middleware/bob.global.ts
@@ -0,0 +1,15 @@
+export default defineNuxtRouteMiddleware((to) => {
+  if (import.meta.client) {
+    if ('itsmebob' in to.query) {
+      window.localStorage.setItem('umami.disabled', '1')
+      window.console.info('Hello, Bob.')
+
+      const { itsmebob, ...otherParams } = to.query
+
+      return navigateTo({
+        path: to.path,
+        query: otherParams
+      }, { replace: true })
+    }
+  }
+})
