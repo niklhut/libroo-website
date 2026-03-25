08e3f09b8be5f3579a932cef5e8bd0ca8797b72d
niklhut <49069026+niklhut@users.noreply.github.com>
2026-03-24 22:25:28 -0500

    refactor: improve 'itsmebob' query parameter handling (#31)

--- app/plugins/itsmebob.client.ts
+++ app/plugins/itsmebob.client.ts
@@ -1,13 +1,7 @@
-export default defineNuxtPlugin({
-  hooks: {
-    'app:beforeMount': async function () {
-      const query = useRoute().query
-      if ('itsmebob' in query) {
-        const { itsmebob, ...otherParams } = query
-        navigateTo({ query: otherParams })
-        window.localStorage.setItem('umami.disabled', '1')
-        window.console.info(`Hello, Bob.`)
-      }
-    }
+export default defineNuxtPlugin(() => {
+  const route = useRoute()
+  if ('itsmebob' in route.query) {
+    window.localStorage.setItem('umami.disabled', '1')
+    window.console.info('Hello, Bob.')
   }
 })
